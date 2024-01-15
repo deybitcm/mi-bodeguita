11d02dbfc71136e39a9dd31f7badade94b1aeccf
davidcm <david000anthony@gmail.com>
2024-01-15 03:42:16 -0500

    Refactor ExploreScreen styling

--- screens/ExploreScreen.jsx
+++ screens/ExploreScreen.jsx
@@ -4,32 +4,32 @@ import React from "react";
 export default function ExploreScreen() {
   return (
     <View style={styles.container}>
-      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+      <View style={styles.elementContainer}>
         <Pressable>
           <Image
             source={require("../assets/empleado.png")}
-            style={{ width: 60, height: 60 }}
+            style={styles.imageContainer}
           />
         </Pressable>
-        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Empleados</Text>
+        <Text style={styles.text}>Empleados</Text>
       </View>
-      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+      <View style={styles.elementContainer}>
         <Pressable>
           <Image
             source={require("../assets/cliente.png")}
-            style={{ width: 60, height: 60 }}
+            style={styles.imageContainer}
           />
         </Pressable>
-        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Clientes</Text>
+        <Text style={styles.text}>Clientes</Text>
       </View>
-      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+      <View style={styles.elementContainer}>
         <Pressable>
           <Image
             source={require("../assets/proveedor.png")}
-            style={{ width: 60, height: 60 }}
+            style={styles.imageContainer}
           />
         </Pressable>
-        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Proveedores</Text>
+        <Text style={styles.text}>Proveedores</Text>
       </View>
     </View>
   );
@@ -40,5 +40,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     paddingTop: 20,
+  },
+  elementContainer: {
+    padding: 10,
+    flex: 1,
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 60,
+    height: 60,
+  },
+  text: {
+    paddingTop: 10,
+    fontWeight: "bold",
   },
 });
