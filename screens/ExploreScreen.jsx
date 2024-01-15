ce77d9cf455bdad4ed6e3cf3f9e9bbcf8f7b6ec0
davidcm <david000anthony@gmail.com>
2024-01-15 02:08:19 -0500

    Add images to ExploreScreen

--- screens/ExploreScreen.jsx
+++ screens/ExploreScreen.jsx
@@ -1,10 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 
 export default function ExploreScreen() {
   return (
     <View style={styles.container}>
-      <Text>ExploreScreen</Text>
+      <View>
+        <Image
+          source={require("../assets/empleado.png")}
+          style={{ width: 30, height: 30 }}
+        />
+      </View>
+      <View>
+        <Image
+          source={require("../assets/cliente.png")}
+          style={{ width: 30, height: 30 }}
+        />
+      </View>
+      <View>
+        <Image
+          source={require("../assets/proveedor.png")}
+          style={{ width: 30, height: 30 }}
+        />
+      </View>
     </View>
   );
 }
@@ -12,7 +29,6 @@ export default function ExploreScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
   },
 });
