a0caa0aec63a29900c2be4f367e5fac649fd6dd9
davidcm <david000anthony@gmail.com>
2024-01-15 02:21:21 -0500

    Add pressable functionality and update styling in ExploreScreen.jsx

--- screens/ExploreScreen.jsx
+++ screens/ExploreScreen.jsx
@@ -1,26 +1,35 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import React from "react";
 
 export default function ExploreScreen() {
   return (
     <View style={styles.container}>
-      <View>
-        <Image
-          source={require("../assets/empleado.png")}
-          style={{ width: 30, height: 30 }}
-        />
+      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+        <Pressable>
+          <Image
+            source={require("../assets/empleado.png")}
+            style={{ width: 60, height: 60 }}
+          />
+        </Pressable>
+        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Empleados</Text>
       </View>
-      <View>
-        <Image
-          source={require("../assets/cliente.png")}
-          style={{ width: 30, height: 30 }}
-        />
+      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+        <Pressable>
+          <Image
+            source={require("../assets/cliente.png")}
+            style={{ width: 60, height: 60 }}
+          />
+        </Pressable>
+        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Clientes</Text>
       </View>
-      <View>
-        <Image
-          source={require("../assets/proveedor.png")}
-          style={{ width: 30, height: 30 }}
-        />
+      <View style={{ padding: 10, flex: 1, alignItems: "center" }}>
+        <Pressable>
+          <Image
+            source={require("../assets/proveedor.png")}
+            style={{ width: 60, height: 60 }}
+          />
+        </Pressable>
+        <Text style={{ paddingTop: 10, fontWeight: "bold" }}>Proveedores</Text>
       </View>
     </View>
   );
@@ -30,5 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
+    paddingTop: 20,
   },
 });
